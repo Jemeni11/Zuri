2c3ae79ea50f5d67f5654199da46e33a58118dd9
Emmanuel C. Jemeni <52603291+Jemeni11@users.noreply.github.com>
2022-06-20 15:16:12 +0100

    Update basic-arithmetic-calculator.js

--- basic-arithmetic-calculator.js
+++ basic-arithmetic-calculator.js
@@ -1,1 +1,15 @@
 // The calculator should be able to perform basic operations like Addition, Subtraction, Multiplication, & Division
+function Calculator(num1, num2, operator){
+    switch (operator) {
+        case '+':
+            return Number(num1) + Number(num2)
+        case '-':
+            return Number(num1) - Number(num2)
+        case '*':
+            return Number(num1) * Number(num2)
+        case '÷':
+            return Number(num1) / Number(num2)  
+        default:
+            return "An invalid operator was entered please try again" 
+    }
+}
